1a5cf0af192aba54cb5d7fbbe05d3204c5cccc3d
kops <jeremie@dudie.fr>
2013-02-17 12:12:31 +0100

    use @Extra to retrieve extra attributes

--- directorysync-android/src/fr/dudie/directorysync/activity/CustomizeSyncActivity.java
+++ directorysync-android/src/fr/dudie/directorysync/activity/CustomizeSyncActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.Extra;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 import fr.dudie.directorysync.R;
@@ -15,23 +16,21 @@ import fr.dudie.directorysync.R;
 @EActivity(R.layout.act_customize_sync)
 public class CustomizeSyncActivity extends FragmentActivity {
 
-    private static final String INTENT_EXTRA_FILENAME = "filename";
-
     public static Intent intent(Context context, String filename) {
         final Intent i = new Intent(context, CustomizeSyncActivity_.class);
-        i.putExtra(INTENT_EXTRA_FILENAME, filename);
+        i.putExtra("filename", filename);
         return i;
     }
 
     @ViewById(R.id.act_customize_sync_filename)
     protected TextView filenameView;
 
-    private String filename;
+    @Extra
+    protected String filename;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        filename = getIntent().getStringExtra(INTENT_EXTRA_FILENAME);
     }
 
     @AfterViews
